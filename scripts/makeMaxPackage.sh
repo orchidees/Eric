8780afb1643ef48e73693730d88e7cfabfcdc368
Daniele Ghisi <danieleghisi@gmail.com>
2018-11-17 15:27:53 +0100

    added first drafts for DoctorMax reference page documentation

--- scripts/makeMaxPackage.sh
+++ scripts/makeMaxPackage.sh
@@ -6,6 +6,11 @@ echo "preparing directories..."
 mkdir -p ~/Documents/Max\ 7/Packages/orchidea/externals/
 mkdir -p ~/Documents/Max\ 7/Packages/orchidea/patchers/
 mkdir -p ~/Documents/Max\ 7/Packages/orchidea/help/
+mkdir -p ~/Documents/Max\ 7/Packages/orchidea/init/
+mkdir -p ~/Documents/Max\ 7/Packages/orchidea/interfaces/
+mkdir -p ~/Documents/Max\ 7/Packages/orchidea/doc/
+mkdir -p ~/Documents/Max\ 7/Packages/orchidea/doc/refpages/
+mkdir -p ~/Documents/Max\ 7/Packages/orchidea/doc/refpages/orchidea_ref/
 mkdir -p ~/Documents/Max\ 7/Packages/orchidea/extras/
 mkdir -p ~/Documents/Max\ 7/Packages/orchidea/docs/
 mkdir -p ~/Documents/Max\ 7/Packages/orchidea/media/
@@ -22,6 +27,15 @@ cp ../max/patchers/*.maxpat ~/Documents/Max\ 7/Packages/orchidea/patchers/
 
 echo "copying help files..."
 cp ../max/help/*.maxhelp ~/Documents/Max\ 7/Packages/orchidea/help/
+
+echo "copying doc files..."
+cp ../max/doc/refpages/orchidea_ref/* ~/Documents/Max\ 7/Packages/orchidea/doc/refpages/orchidea_ref/
+
+echo "copying init files..."
+cp ../max/init/* ~/Documents/Max\ 7/Packages/orchidea/init/
+
+echo "copying interface files..."
+cp ../max/interfaces/* ~/Documents/Max\ 7/Packages/orchidea/interfaces/
 
 echo "copying targets..."
 cp ../max/media/* ~/Documents/Max\ 7/Packages/orchidea/media/
